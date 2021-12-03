9a6fe77b42b69028683b06d035f981b3b35ffa37
mlee1425 <94021759+mlee1425@users.noreply.github.com>
2021-12-03 18:51:51 -0500

    This push has the following:
    -A creation of the Register Page class and Register Page FXML doc.
    
    -A creation of the LoginPage FXML doc.
    
    -An update of the LoginPage class

--- BAM Airlines/src/edu/gsu/gui/LoginPage.java
+++ BAM Airlines/src/edu/gsu/gui/LoginPage.java
@@ -1,13 +1,18 @@
 package edu.gsu.gui;
 
 import java.util.HashMap;
+
 import java.util.HashSet;
+
+import java.io.IOException;
+
 
 import edu.gsu.common.Action;
 import edu.gsu.common.Customer;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;  
 import javafx.scene.control.Alert;
@@ -16,12 +21,51 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.*;
+
+
+
 
 public class LoginPage extends Application {
 
+  @FXML
+  private TextField username;
+  @FXML
+  private TextField password;
+  @FXML
+  private Button logIn;
+  @FXML 
+  private Button signUp;
+  @FXML
+  private Button register;
+  @FXML
+  private Button forgotPassword;
+  @FXML 
+  private AnchorPane LoginScreen;
+  
+  @FXML
+  public void loginClicked(MouseEvent event) throws IOException {
+	  Customer c = new Customer();
+	  if (username.getText().isEmpty() && password.getText().isEmpty() 
+			  || username.getText().isEmpty()
+			  || username.getText().isEmpty()) {
+		  Alerts.alert1("Please enter username and/or password");
+	  }
+	  else (
+			 System.out.println("");
+  }
   
   
-  Button loginButton = new Button("Login");
+  
+  
+  
+  
+  
+  
+  
+  
   
   @Override // Override the start method in the Application class
   public void start(Stage primaryStage) {
@@ -35,7 +79,8 @@ public class LoginPage extends Application {
     
     loginButton.setOnAction(e -> login(userName.getText(), password.getText()));
     
-    pane.getChildren().addAll(userName, password, loginButton);
+    
+    pane.getChildren().addAll(userName, password, logIn);
     
     // Create a scene and place it in the stage
     Scene scene = new Scene(pane, 500, 100);
@@ -60,7 +105,7 @@ public class LoginPage extends Application {
     
   }
   
-  public void login(String userName, String password)  {
+  public void signup(String userName, String password)  {
 	  
 	  Customer customer = new Customer();
 	  
@@ -81,7 +126,7 @@ public class LoginPage extends Application {
 		  
 		  if (success) {
 	  
-			  Stage stage = (Stage) loginButton.getScene().getWindow();
+			  Stage stage = (Stage) logIn.getScene().getWindow();
 			  
 	  
 			  stage.close();
--- BAM Airlines/src/module-info.java
+++ BAM Airlines/src/module-info.java
@@ -1,6 +1,8 @@
 module BAM {
 	requires javafx.controls;
 	requires java.sql;
+	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
