e01b1f1f4fde9e9e521760e361e962c5391af018
biruk <blare@10.0.0.156>
2021-12-05 11:28:45 -0500

    pushing updated connection to database, should work for everyone

---
+++ BAM Airlines/src/edu/gsu/db/ConnectDatbase.java
@@ -0,0 +1,33 @@
+package edu.gsu.db;
+
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectDatbase {
+		public static void main(String[] args) throws Exception {
+			// TODO Auto-generated method stub
+			getConnection();
+		}
+
+		public static Connection getConnection() throws Exception {
+			try {
+				String driver = "com.mysql.cj.jdbc.Driver";
+				String url = "jdbc:mysql://10.0.0.103:3306/bamdb";
+				String username = "root";
+				String password = "Java123*";
+				Class.forName(driver);
+				
+				Connection conn = DriverManager.getConnection(url, username, password);
+				System.out.println("Connceted");
+				return conn;
+			}	catch(Exception e) {System.out.print(e);
+			
+			
+			
+			return null;
+			
+	}
+
+		}
+}
